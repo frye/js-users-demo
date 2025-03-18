6c0d1b6b242e456db41931e79f5b6f9435b92242
Samuli Jarvinen <frye@github.com>
2025-03-18 08:44:18 -0700

    Add user deletion route and expand test users list

--- src/users.js
+++ src/users.js
@@ -30,6 +30,97 @@ let users = [
     name: 'Eve Eerie',
     emoji: '🧙',
   },
+  // Add more test users from star wars
+  {
+    id: uuidv4(),
+    name: 'Luke Skywalker',
+    emoji: '🌌',
+  },
+  {
+    id: uuidv4(),
+    name: 'Leia Organa',
+    emoji: '👑',
+  },
+  {
+    id: uuidv4(),
+    name: 'Han Solo',
+    emoji: '🚀',
+  },
+  {
+    id: uuidv4(),
+    name: 'Chewbacca',
+    emoji: '🐻',
+  },
+  {
+    id: uuidv4(),
+    name: 'Darth Vader',
+    emoji: '👺',
+  },
+  {
+    id: uuidv4(),
+    name: 'Yoda',
+    emoji: '👽',
+  },
+  {
+    id: uuidv4(),
+    name: 'R2-D2',
+    emoji: '🤖',
+  },
+  {
+    id: uuidv4(),
+    name: 'C-3PO',
+    emoji: '🤖',
+  },
+  {
+    id: uuidv4(),
+    name: 'Obi-Wan Kenobi',
+    emoji: '👴',
+  },
+  {
+    id: uuidv4(),
+    name: 'Padmé Amidala',
+    emoji: '👸',
+  },
+  {
+    id: uuidv4(),
+    name: 'Anakin Skywalker',
+    emoji: '👦',
+  },
+  {
+    id: uuidv4(),
+    name: 'Mace Windu',
+    emoji: '👴',
+  },
+  {
+    id: uuidv4(),
+    name: 'Boba Fett',
+    emoji: '👦',
+  },
+  {
+    id: uuidv4(),
+    name: 'Jabba the Hutt',
+    emoji: '🐍',
+  },
+  {
+    id: uuidv4(),
+    name: 'Darth Maul',
+    emoji: '👹',
+  },
+  {
+    id: uuidv4(),
+    name: 'Lando Calrissian',
+    emoji: '🎩',
+  },
+  {
+    id: uuidv4(),
+    name: 'Finn',
+    emoji: '👦',
+  },
+  {
+    id: uuidv4(),
+    name: 'Rey',
+    emoji: '👧',
+  },
 ];
 
 // Export the users array
